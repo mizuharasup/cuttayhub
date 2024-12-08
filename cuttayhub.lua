50f847d2fba4df828019a4444be85fa67312a697
Mizuhara <167328143+mizuharasup@users.noreply.github.com>
2024-12-08 16:38:45 +0700

    Update cuttayhub.lua

--- cuttayhub.lua
+++ cuttayhub.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Có Cái Lồn Địt Mẹ Mày Tham Lam Vừa Thằng Đầu Cặc")
+game.Players.LocalPlayer:Kick("Có Cái Lồn Địt Mẹ Mày Tham Lam Vừa Thằng Đầu Cặc đưa tao 100k tao cho test bú free ít thôi VCB BUI QUY DIEM 1051099160 ")
